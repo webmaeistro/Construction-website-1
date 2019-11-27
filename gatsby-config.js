7f47df5d629489585558b3bdd042428b33369cfb
Martin Andersen <martin.andersn@gmail.com>
2019-11-27 22:03:45 +0100

    1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,44 +1,43 @@
 module.exports = {
   siteMetadata: {
-    title: `TP Construction`,
-    fullTitle: `TANPEL CONSTRUCTION`,
-    description: `Listen better | Plan better | Build better.`,
-    about: `Since our incorporation in 1981, we've grown from a regional organization to a multi-faceted, national presence in the industry with headquarters in New York.`,
-    email: `info@tanpelconstruction.com`,
+    title: `Danielsen Bygg`,
+    fullTitle: `Danielsen Bygg Horten`,
+    description: `Forstår deg bedere | Planlegger bedere | Konstruerer det best.`,
+    about: `Danielsen Bygg ble startet opp i Oktober 2018, av snekkeren Morten Danielsen, fra Horten. Morten har god erfaring med alt som har med snekkerarbeid å gjøre. Selskapet tar på seg jobber som totalrenovering, tilbygg, tak, malejobber, og alt annet snekkerarbeid. 
+    Ta kontakt på epost eller på mobil å få gratis befaring! `,
+    email: `danielsen.bygg@gmail.com`,
     social: {
-      facebook: `https://facebook.com/tanpelconstruction`,
-      twitter: `https://twitter.com/tanpelconstruction`,
-      instagram: `https://instagram.com/tanpelconstruction`,
-      linkedin: `https://linkedin.com/in/tanpelconstruction`,
+      facebook: `https://facebook.com/tDanielsenByggHorten`,
+      twitter: `https://twitter.com/tDanielsenByggHorten`,
+      instagram: `https://instagram.com/tDanielsenByggHorten`,
+      linkedin: `https://linkedin.com/in/tDanielsenByggHorten`,
     },
     contact: {
-      mobile: `(+63) 912 345 6789`,
-      telephone: `(02) 123 4567`,
+      mobile: `+47 984 80 400`,
+      telephone: `+47 984 80 400`,
     },
-    address: `Bermuda Subd., Pamplona 3, Las Pinas City, Philippines, 1740`,
-    map: `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3863.590531801578!2d120.92931831483857!3d14.450745589898093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397d282c1782f4f%3A0x62e247641b50bd21!2sBermuda%20Country%20Subdivision%2C%20Bacoor%2C%20Cavite!5e0!3m2!1sen!2sph!4v1566970336076!5m2!1sen!2sph`,
+    address: `sentroveien 26, 3182 Horten`,
+    map: `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2030.3411658252485!2d10.475396116075583!3d59.41070378168737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4646b4b73f55a787%3A0x5e7a00a641159e2c!2sSentroveien%2026%2C%203182%20Horten!5e0!3m2!1sno!2sno!4v1574888394763!5m2!1sno!2sno`,
     opening: {
       day: `Monday - Friday`,
       hour: `8:00am - 5:00pm`,
     },
     careers: {
-      title: `Benefits and rewards`,
-      about: `Our diverse capabilities provide employees with the ability to
-      work on projects of all sizes and types and receive amazing
-      benefits in return. They include but are in no way limited to:`,
+      title: `Danielsen Bygg `,
+      about: `Tar ingen ansetta på dette tidspunkettet!`,
       benefits: [
-        `Healthcare and wellness benefits`,
-        `Retirement and financial protection`,
-        `Employee development programs`,
+        `her lister vi hvorfor jobbe for deg`,
+        `du er lokal gutt og fleksibel`,
+        `Billig og høy kvalitet levert til tide`,
       ],
     },
 
     author: {
-      name: `Mark Dino Pelonia`,
+      name: `Martin Andersen`,
       position: `Web Developer`,
-      email: `markdinopelonia447@gmail.com`,
-      contact: `(+63) 946 290 9678`,
-      website: `https://markdino.github.io/portfolio`,
+      email: `marander@pm.me`,
+      contact: `+47 99347544 `,
+      website: `github.com/webmaeistro`,
     },
   },
   plugins: [
@@ -71,8 +70,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `danielsen-bygg-horten`,
+        short_name: `danielsenbygg`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
@@ -82,6 +81,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+     `gatsby-plugin-offline`,
   ],
 }
